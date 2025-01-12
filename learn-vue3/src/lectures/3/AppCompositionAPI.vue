08ec203ed8bcf7ffd2261a13fe4560442a48f1ae
x3249444@gmail.com <x3249444@gmail.com>
2025-01-12 22:26:25 +0900

    all

---
+++ learn-vue3/src/lectures/3/AppCompositionAPI.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<button v-on:click="increment">counter : {{ counter }}</button>
+	</div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+export default {
+	setup() {
+		const counter = ref(0);
+		const increment = () => {
+			counter.value++;
+		};
+
+		onMounted(() => {
+			console.log('마운트됨');
+		});
+
+		return {
+			counter,
+			increment,
+			onMounted,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
