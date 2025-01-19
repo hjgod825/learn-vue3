f876e06300f760ecfba04741b5db65c547943927
x3249444@gmail.com <x3249444@gmail.com>
2025-01-19 21:34:07 +0900

    9 style biindding 배움

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -1,20 +1,9 @@
 <template>
-	<div>
-		<div>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nostrum
-			quo provident. Recusandae, alias temporibus asperiores dolore aperiam
-			optio ab et, totam molestiae quidem architecto reprehenderit sunt tempore
-			mollitia iste.
-		</div>
-	</div>
+	<div></div>
 </template>
 
 <script>
-export default {
-	setup() {
-		return {};
-	},
-};
+export default {};
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/lectures/9/App_StyleBinding.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nostrum
+			quo provident. Recusandae, alias temporibus asperiores dolore aperiam
+			optio ab et, totam molestiae quidem architecto reprehenderit sunt tempore
+			mollitia iste.
+		</div>
+		<button @click="fontSize--">-</button>
+		<button @click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '30px',
+		// });
+
+		const fontSize = ref(13);
+
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+
+		return { styleObject, fontSize };
+	},
+};
+</script>
+
+<style scoped></style>
